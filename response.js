57578cdfe3519b851bfb14be1b1afe9a8b87c0e5
Christian Beedgen <raychaser@gmail.com>
2015-03-12 17:48:17 -0700

    Parallel hierarchy named after the container ID

--- response.js
+++ response.js
@@ -1,3 +1,5 @@
+var fs = require('fs')
+
 module.exports = function(root, verbose, body){
 
   function S4() {
@@ -20,18 +22,34 @@ module.exports = function(root, verbose, body){
     PowerstripProtocolVersion:1
   }
 
-  if(body.Type == 'pre-hook'){
+  if (body.Type == 'pre-hook'){
 
     // Figure out the actual request to the Docker daemon.
     var clientRequestBody = JSON.parse(body.ClientRequest.Body)
 
-    debug("\n\nbody.ClientRequest.Body:\n")
+    debug("\nbody.ClientRequest.Body:")
     debug(JSON.stringify(clientRequestBody, null, 4))
-    debug("\n\n")
 
     // Get the ID of the container, and the name.
     var token = createGuid()
-    var pathPrefix = root + "/" + token
+    var pathPrefix = root + "/original/" + token
+
+    // Create the directory for the original logs
+    fs.stat(pathPrefix, function(err, stat) {
+      if (err) {
+        fs.mkdir(pathPrefix, function (err) {
+          if (err) {
+            setTimeout(function() {
+              if (err) throw err;
+              debug("Making original directory complete")
+            }, 1000)
+          }
+          else {
+            debug("Making original directory complete")
+          }
+        })
+      }
+    })
 
     // Figure out all the log file paths from the LOGS env
     var envs = clientRequestBody.Env || []
@@ -68,20 +86,76 @@ module.exports = function(root, verbose, body){
     var modifiedClientRequestBody = JSON.stringify(clientRequestBody)
     body.ClientRequest.Body = modifiedClientRequestBody
     returnBody.ModifiedClientRequest = body.ClientRequest
-  // }
-  // else if(body.Type == 'post-hook'){
+  }
+  else if(body.Type == 'post-hook'){
 
-    // var serverResponseBody = JSON.parse(body.ServerResponse.Body)
+    // Get the modified client request 
+    var modifiedClientRequestBody = JSON.parse(body.ModifiedClientRequest.Body)
+    debug("\nbody.ModifiedClientRequest.Body:")
+    debug(JSON.stringify(modifiedClientRequestBody, null, 4))
 
-    //
-    // Once we know how to thread the modified client request 
-    // through to the post-hook, use the token to rename the 
-    // root directory for the bind mounts to the ID of the container.
-    //
+    // From the modified client request we can extract
+    // the token we put there
+    var envs = modifiedClientRequestBody.Env || []
+    debug("Envs: " + envs)
+    var token = null
+    for (var i = 0; i < envs.length; i++) {
+      var env = envs[i]
+      if (env.startsWith(POWERSTRIP_TOKEN)) {
+        var kv = env.split("=", POWERSTRIP_TOKEN.length + 1)
+        if (kv[0] === POWERSTRIP_TOKEN) {
+          token = kv[1]
+        }
+      }
+    }
+    debug("Token: " + token)
 
-    // var modifiedServerResponseBody = JSON.stringify(serverResponseBody)
-    // body.ServerResponse.Body = modifiedServerResponseBody
-    // returnBody.ModifiedServerResponse = body.ServerResponse
+    // Get the server response
+    var serverResponseBody = JSON.parse(body.ServerResponse.Body)
+    debug("body.ServerResponse.Body:")
+    debug(JSON.stringify(serverResponseBody, null, 4))
+
+    // From the server response we can get the container ID
+    var containerId = serverResponseBody.Id
+    debug("ContainerId: " + containerId)
+
+    // With the token, we now know the path to rename
+    var originalPath = root + "/original/" + token
+    debug("OriginalPath: " + originalPath)
+    var containersRoot = root + "/containers"
+    var containerPath = containersRoot + "/" + containerId
+    debug("ContainerPath: " + containerPath)
+
+    // Make sure the root to the containers directory exists
+    fs.stat(containersRoot, function(err, stat) {
+      if (err) {
+        fs.mkdir(containersRoot, function (err) {
+          if (err) throw err;
+          debug("Making containers directory complete")
+        })
+      }
+    })
+
+    // Finally, create the symlink.
+    fs.symlink(originalPath, containerPath, function (err) {
+      if (err) {
+        debug("Error during linking, trying again: " + err)
+        setTimeout(function() {
+            fs.symlink(originalPath, containerPath, function (err) {
+              if (err) throw err;
+              debug("Linking complete")
+            })
+        }, 1000)
+      } 
+      else {
+        debug("Linking complete")        
+      }
+    })
+
+    // Just send back the incoming server response.
+    var modifiedServerResponseBody = JSON.stringify(serverResponseBody)
+    body.ServerResponse.Body = modifiedServerResponseBody
+    returnBody.ModifiedServerResponse = body.ServerResponse
   
   } else{
 
