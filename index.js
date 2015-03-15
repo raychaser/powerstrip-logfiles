5e8d59def9572f799e8128b2300655eb16c8c121
Christian Beedgen <raychaser@gmail.com>
2015-03-15 12:57:52 -0700

    More cleanup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var http = require('http');
+
+// Commandline argument parsing
 var args = require('minimist')(process.argv, {
   alias:{
     p: 'port',
@@ -12,24 +14,29 @@ var args = require('minimist')(process.argv, {
   string: ['root']
 })
 
+// Make sure a root path is specified
 if (!args.root || args.root == "") {
   console.log("\nNo --root specified. Exiting.\n\n");
   process.exit();
 }
 
+// Check for absolute root path
 if (!args.root.startsWith("/")) {
   console.log("\nOnly absolute paths allowed for --root. Exiting.\n\n");
   process.exit();
 }
 
+// Make sure we exit on CTRL-C
 process.on('SIGINT', function() {
   process.exit();
 })
 
+// Create the HTTP server
 var Server = require('./server');
 console.log("Root file path: " + args.root);
 var server = http.createServer(Server(args));
 
+// Start listening for HTTP requests
 server.listen(args.port, function() {
   console.log('Server listening on port: ' + args.port);
   if (args.verbose){
